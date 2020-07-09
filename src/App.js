9159daa51873a70cd1adb3744a0ad7e4043eda0d
Christian Montero <cmontero@bu.edu>
2020-07-09 11:59:07 -0400

    test pre-deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { HashLink as Link } from 'react-router-hash-link';
 
 import Container from 'react-bootstrap/Container';
 
