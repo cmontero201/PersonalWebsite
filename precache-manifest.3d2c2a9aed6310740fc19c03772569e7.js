45af044a40d9d13bda0c8b56473819465b96a3dd
Christian Montero <cmontero@bu.edu>
2020-07-09 12:05:12 -0400

    Updates

---
+++ precache-manifest.3d2c2a9aed6310740fc19c03772569e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935a39064d71a36442186e25406fd6a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc974172f459938554c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c966810fb199b37cb966",
+    "url": "/static/css/main.b2b34efb.chunk.css"
+  },
+  {
+    "revision": "2dc974172f459938554c",
+    "url": "/static/js/2.e8daaa5a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e8daaa5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c966810fb199b37cb966",
+    "url": "/static/js/main.2fa4a2fc.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  },
+  {
+    "revision": "662d42323b8402277f5e181c03206d32",
+    "url": "/static/media/self.662d4232.png"
+  }
+]);
